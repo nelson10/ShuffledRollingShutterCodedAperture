6aa824ead84e5090e11922128573aa896beec3d1
nelson10 <38512291+nelson10@users.noreply.github.com>
2021-09-14 14:27:28 -0500

    Add files via upload

--- main_real_measurements.m
+++ main_real_measurements.m
@@ -11,28 +11,25 @@ addpath(genpath('./results'));
 addpath(genpath('./CodedApertureDesignOptimal'));
 addpath(genpath('./dataset'))
 addpath(genpath('./Reconstruction'))
+addpath(genpath('./real'))
 
 %profile on
 N = 256;  % spatial size
-
-
-load('wine.mat') % real measurements
+%% real measurements
+load('wine.mat') 
 Y = measO_roi;
 mask = opt_mask;
-%Y = sampling(data,mask,N); % compute compressive measurements with designed mask
 figure(1)
 imagesc(Y), title('Compressive Measurements using Designed Coded Apertures')
 colormap('gray')
 
 [J] = increaseExposure(Y,mask,N);
 
-% Reconstruction using designed coded apertures
+%% Reconstruction using designed coded apertures
 [Xrec] = interpolation(J);
 disp('Interpolation of Temporal Rolling Shutter Compressive Measurements Captured using Designed Coded Apertures')
-%[p,s,r] = metrics(data,Xrec);
 
 implay(Xrec./max(Xrec(:))),title('Interpolation with Designed Coded Apertures')
-
 Y = measR_roi;
 mask = rnd_mask;
 
@@ -41,8 +38,7 @@ imagesc(Y), title('Compressive Measurements using Random Coded Apertures')
 colormap('gray')
 
 [J] = increaseExposure(Y,mask,N);
-% Reconstruction using random coded apertures
+%% Reconstruction using random coded apertures
 [Xrec] = interpolation(J);
 disp('Interpolation of Temporal Rolling Shutter Compressive Measurements Captured using Random Coded Apertures')
-%[p,s,r] = metrics(data,Xrec);
 implay(Xrec./max(Xrec(:))),title('Interpolation with Random Coded Apertures')
