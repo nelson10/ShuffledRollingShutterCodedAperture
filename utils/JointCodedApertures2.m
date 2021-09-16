a47b577563aa5d21aa329b89879744429a63fc1a
nelson10 <38512291+nelson10@users.noreply.github.com>
2021-09-16 10:53:43 -0500

    Add files via upload

--- utils/JointCodedApertures2.m
+++ utils/JointCodedApertures2.m
@@ -1,14 +1,16 @@
-function [X] = JointCodedApertures2(Y,mask,N)
+function [J,gt] = JointCodedApertures2(Y,mask,N,nnz,data)
 
 for j=1:N
     J(:,:,j) = ((mask(:,:,j).*Y))./255; 
 end
 
-nm = round(0.033*(N))
+nm = round(nnz*(N))
 N1 = N/nm;
 for jj=1:N1
     dmd(:,:,jj) = sum(mask(:,:,1+(jj-1)*(nm):(jj)*nm),3);
     X(:,:,jj) = sum(J(:,:,1+(jj-1)*(nm):(jj)*nm),3);
+    gt1(:,:,jj) = sum(data(:,:,1+(jj-1)*(nm):(jj)*nm),3);
+    gt(:,:,jj) = gt1(:,:,jj)./max(gt1(:));
 end
 X = X./max(X(:));
 sample =100*(sum(dmd(:))/numel(dmd));
