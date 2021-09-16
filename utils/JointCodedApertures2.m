043c3809b587916bf0625266f135a091ae318c06
nelson10 <38512291+nelson10@users.noreply.github.com>
2021-09-16 11:56:18 -0500

    Add files via upload

--- utils/JointCodedApertures2.m
+++ utils/JointCodedApertures2.m
@@ -1,5 +1,4 @@
-function [J,gt] = JointCodedApertures2(Y,mask,N,nnz,data)
-
+function [X,gt] = JointCodedApertures2(Y,mask,N,nnz,data)
 for j=1:N
     J(:,:,j) = ((mask(:,:,j).*Y))./255; 
 end
@@ -12,6 +11,7 @@ for jj=1:N1
     gt1(:,:,jj) = sum(data(:,:,1+(jj-1)*(nm):(jj)*nm),3);
     gt(:,:,jj) = gt1(:,:,jj)./max(gt1(:));
 end
+
 X = X./max(X(:));
 sample =100*(sum(dmd(:))/numel(dmd));
 disp("number of non-zero values " + num2str(sample) + "%");
